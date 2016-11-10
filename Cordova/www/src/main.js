8642d148acbbb407be432ac31b78fb54a75f39e9
Ryan O'Day <romanguy722@gmail.com>
2016-11-10 13:59:23 -0500

    Initial Commit

---
+++ Cordova/www/src/main.js
@@ -0,0 +1,203 @@
+var canvas, ctx;
+var WIDTH, HEIGHT;
+var points = [];
+var running;
+var canvasMinX, canvasMinY;
+var doPreciseMutate;
+
+var POPULATION_SIZE;
+var ELITE_RATE;
+var CROSSOVER_PROBABILITY;
+var MUTATION_PROBABILITY;
+var OX_CROSSOVER_RATE;
+var UNCHANGED_GENS;
+
+var mutationTimes;
+var dis;
+var bestValue, best;
+var currentGeneration;
+var currentBest;
+var population;
+var values;
+var fitnessValues;
+var roulette;
+var lett;
+
+$(function() {
+  init();
+  initData();
+  addRandomPoints(getRandomIntInclusive(8,12));
+  $('#new_btn').click(function() {
+    running === false;
+    initData();
+    points = new Array();
+    addRandomPoints(getRandomIntInclusive(8,12));
+    $('#status').text("");
+    running = false;
+  });
+  $('#start_btn').click(function() { 
+    if(points.length >= 3) {
+      initData();
+      GAInitialize();
+      running = true;
+    } else {
+      alert("Not enough points!");
+    }
+  });
+  $('#clear_btn').click(function() {
+    running === false;
+    initData();
+    points = new Array();
+  });
+});
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function init() {
+  ctx = $('#canvas')[0].getContext("2d");
+  WIDTH = $('#canvas').width();
+  HEIGHT = $('#canvas').height();
+  setInterval(draw, 10);
+  init_mouse();
+}
+function init_mouse() {
+  $("canvas").click(function(evt) {
+    if(!running) {
+      canvasMinX = $("#canvas").offset().left;
+      canvasMinY = $("#canvas").offset().top;
+      $('#status').text("");
+
+      x = evt.pageX - canvasMinX;
+      y = evt.pageY - canvasMinY;
+      points.push(new Point(x, y));
+    }
+  });
+}
+function initData() {
+  running = false;
+  POPULATION_SIZE = 30;
+  ELITE_RATE = 0.3;
+  CROSSOVER_PROBABILITY = 0.9;
+  MUTATION_PROBABILITY  = 0.01;
+  //OX_CROSSOVER_RATE = 0.05;
+  UNCHANGED_GENS = 0;
+  mutationTimes = 0;
+  doPreciseMutate = true;
+
+  bestValue = undefined;
+  best = [];
+  currentGeneration = 0;
+  currentBest;
+  population = []; //new Array(POPULATION_SIZE);
+  values = new Array(POPULATION_SIZE);
+  fitnessValues = new Array(POPULATION_SIZE);
+  roulette = new Array(POPULATION_SIZE);
+}
+function addRandomPoints(number) {
+  running = false;
+  for(var i = 0; i<number; i++) {
+    points.push(randomPoint());
+  }
+}
+function drawCircle(point) {
+  ctx.fillStyle = '#000';
+  ctx.beginPath();
+  ctx.arc(point.x, point.y, 3, 0, Math.PI*2, true);
+  ctx.closePath();
+  ctx.fill();
+  ctx.fillText(lett, point.x + 5, point.y);
+}
+function getLetter(index){
+  switch (index){
+    case 0:
+      return "A";
+    case 1:
+      return "B";
+    case 2:
+      return "C";
+    case 3:
+      return "D";
+    case 4:
+      return "E";
+    case 5:
+      return "F";
+    case 6:
+      return "G";
+    case 7:
+      return "H";
+    case 8:
+      return "I";
+    case 9:
+      return "J";
+    case 10:
+      return "K";
+    case 11:
+      return "L";
+    case 12:
+      return "M";
+    case 13:
+      return "N";
+    case 14:
+      return "O";
+    case 15:
+      return "P";
+    case 16:
+      return "Q";
+    case 17:
+      return "R";
+    case 18:
+      return "S";
+    case 19:
+      return "T";
+    case 20:
+      return "U";
+    case 21:
+      return "V";
+    case 22:
+      return "W";
+    case 23:
+      return "X";
+    case 24:
+      return "Y";
+    case 25:
+      return "Z";
+    default:
+      return "0";
+  }
+}
+function drawLines(array) {
+  ctx.strokeStyle = '#f00';
+  ctx.lineWidth = 1;
+  ctx.beginPath();
+
+  ctx.moveTo(points[array[0]].x, points[array[0]].y);
+  for(var i=1; i<array.length; i++) {
+    ctx.lineTo( points[array[i]].x, points[array[i]].y )
+  }
+  ctx.lineTo(points[array[0]].x, points[array[0]].y);
+
+  ctx.stroke();
+  ctx.closePath();
+}
+function draw() {
+  if(running) {
+    GANextGeneration();
+  } else {
+    $('#status').text("Points: " + points.length)
+  }
+  clearCanvas();
+  if (points.length > 0) {
+    for(var i=0; i<points.length; i++) {
+      lett = getLetter(i);
+      drawCircle(points[i]);
+    }
+    if(best.length === points.length) {
+      drawLines(best);
+    }
+  }
+}
+function clearCanvas() {
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+}
